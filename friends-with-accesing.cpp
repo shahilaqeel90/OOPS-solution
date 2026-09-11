d166ab0575cbbe18b905e46d87bac3034ded3af7
shahilaqeel90 <shahil.25b15410307@abes.ac.in>
2026-09-11 13:32:56 +0530

    friends-function

---
+++ friends-with-accesing.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class employee{
+    private:
+    int salary;
+    public:
+    string name;
+    employee(string n,int s){
+        name=n;
+        salary=s;
+    }
+    
+}
